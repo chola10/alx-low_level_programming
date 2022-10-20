010a16bd3c3f62751c961119871f1f47d08971f5
chola10 <cholakabaso10@gmail.com>
2022-10-20 06:29:42 -0700

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int a;
+
+	printf("infinite loop incoming :(\n");
+	a = 0;
+	/**
+	 * while (a > 10)
+	 * {
+	 * 	putchar(a);
+	 * }
+	 */
+	printf("infinite loop avoided :\\o/\n");
+
+	return (0);
+}
