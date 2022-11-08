ee969597956df4923035b9f75ff1b76b970cbbd8
chola10 <cholakabaso10@gmail.com>
2022-11-08 07:12:04 -0800

    removed directory

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print if the number is positive, negative or 0
+ * Return: Always 0 
+ */
+int main(void)
+{
+	int a;
+
+	$rand(time(0));
+	a = rand() - RAMD_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", a);
+	}
+	else if (a == 0)
+	{
+		printf("%d is zero\n", a);
+	}
+	else
+	{
+		printf("%d is negative\n", a);
+	}
+	return (0);
+}
+	
